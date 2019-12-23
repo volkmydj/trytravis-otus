ae1c37ac722aa3513780dd37b68b4ccfbe4f52b1
Aleksey Volkov <aleksey.marantz@gmail.com>
2019-12-23 13:16:17 +0300

    Add sudo directive

--- startup.sh
+++ startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-apt update
-apt install -y ruby-full ruby-bundler build-essential
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
-bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.0.list'
-apt update
-apt install -y mongodb-org
-systemctl enable mongod.service
-systemctl start mongod.service
-systemctl status mongod
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.0.list'
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl enable mongod.service
+sudo systemctl start mongod.service
+sudo systemctl status mongod
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit
 bundle install
