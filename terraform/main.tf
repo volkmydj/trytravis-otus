87fec51ddca9265ab2415d03b24e55b06dcdbb59
volkmydj <aleksey.marantz@gmail.com>
2019-12-28 14:25:13 -0800

    Сделано основное задание

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,23 +8,57 @@ provider "google" {
   version = "2.15"
 
   #ID project
-  project = "devops-course-1"
+  project = var.project
+  # Credentials
+  #credentials = "${file("Infra-17bbabde7981.json")}"
 
-  region = "europe-west-1"
+  region = var.region
 }
 
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
-  zone         = "europe-west1-b"
+  zone         = var.zone
   boot_disk {
     initialize_params {
-      image = "reddit-base"
+      image = var.disk_image
     }
   }
 
-  network_interface {
-    network       = "default"
-    access_config = {}
+  metadata = {
+    ssh-keys = "appuser:${file(var.public_key_path)}\nappuser2:${file(var.public_key_path)}\nappuser3:${file(var.public_key_path)}"
   }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+  tags = ["reddit-app"]
+
+  connection {
+    type        = "ssh"
+    host        = self.network_interface[0].access_config[0].nat_ip
+    user        = "appuser"
+    agent       = false
+    private_key = file(var.private_key)
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
 }
