95363cbcb6a3d0394bff8e8a28161f3ca34c653f
volkmydj <aleksey.marantz@gmail.com>
2020-01-06 04:39:36 -0800

    Check Test 2

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,13 +1,35 @@
 provider "google" {
-  version = "2.5.0"
+  version = "~> 2.5"
   project = "${var.project}"
   region  = "${var.region}"
 }
 
+# module "storage-bucket-stage" {
+#   source   = "git::https://github.com/SweetOps/terraform-google-storage-bucket.git?ref=master"
+#   name     = "storage-bucket-stage"
+#   stage    = "stage"
+#   location = "europe-west1"
+# }
+#
+# module "storage-bucket-prod" {
+#   source   = "git::https://github.com/SweetOps/terraform-google-storage-bucket.git?ref=master"
+#   name     = "storage-bucket-prod"
+#   stage    = "production"
+#   location = "europe-west1"
+# }
+#
+# output storage-stage-bucket_url {
+#   value = "${module.storage-bucket-stage.url}"
+# }
+#
+# output storage-prod-bucket_url {
+#   value = "${module.storage-bucket-prod.url}"
+# }
+
 module "storage-bucket" {
   source  = "SweetOps/storage-bucket/google"
   version = "0.1.1"
-  name    = ["storage-bucket-volkmydj", "storage-bucket-volkmydj2"]
+  name    = ["storage-bucket-otus-stage", "storage-bucket-otus-prod"]
 }
 
 output storage-bucket_url {
