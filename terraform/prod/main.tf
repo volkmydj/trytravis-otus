02ae7517dc8fa228beba93611f6b1dc523af0925
volkmydj <aleksey.marantz@gmail.com>
2020-01-09 02:52:17 -0800

    Исправлены ошибки в файлах

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -9,23 +9,23 @@ provider "google" {
   region  = var.region
 }
 
+module "db" {
+  source          = "../modules/db"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  db_disk_image   = "${var.db_disk_image}"
+}
+
 module "app" {
   app_name          = "${var.app_name}"
   source            = "../modules/app"
   public_key_path   = "${var.public_key_path}"
   zone              = "${var.zone}"
   app_disk_image    = "${var.app_disk_image}"
-  db_external_ip    = "${module.db.db_external_ip}"
-  with_provisioning = "true"
+  db_reddit_ip      = "${module.db.internal_ip}"
+  provision_enabled = true
 }
 
-module "db" {
-  source          = "../modules/db"
-  public_key_path = "${var.public_key_path}"
-  zone            = "${var.zone}"
-  db_disk_image   = "${var.db_disk_image}"
-  app_external_ip = "${module.app.app_external_ip}"
-}
 
 module "vpc" {
   source        = "../modules/vpc"
