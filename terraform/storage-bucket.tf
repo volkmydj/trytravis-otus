f61afdfc2e8789d22e2f730d8f025035aabacbd3
Aleksey Volkov <aleksey.marantz@gmail.com>
2020-01-12 05:30:25 -0800

    Test 2

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,10 +1,10 @@
-terraform {
-  #Terraform version
-  required_version = "~> 0.12.19"
-}
+# terraform {
+#   #Terraform version
+#   required_version = "~> 0.12.19"
+# }
 
 provider "google" {
-  version = "~> 2.5.0"
+  #version = "~> 2.5.0"
   project = var.project
   region  = var.region
 }
